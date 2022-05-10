0995ea5bd3e5d9c45ebabb7749ec6fb9b6f41533
Roxane Jacob <roxane.jacob2@gmail.com>
2022-05-10 14:26:25 +0200

    Running version

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from utils import two_dim_visualization
 
 if __name__ == '__main__':
 
-    # toydata = pd.read_csv('data/toydata_tiny.csv')
-    # X, y = np.array(toydata[['x1', 'x2']]), np.array(toydata['y'])
+    #toydata = pd.read_csv('data/toydata_tiny.csv')
+    #X, y = np.array(toydata[['x1', 'x2']]), np.array(toydata['y'])
 
     toydata = pd.read_csv('data/toydata_large.csv')
     X, y = np.array(toydata[['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8']]), np.array(toydata['y'])
@@ -74,19 +74,19 @@ if __name__ == '__main__':
     print('\n--- Compute non-linear features ---')
     start = time.time()
     nlf = NonLinearFeatures(m=50, sigma=2.0)
-    X = nlf.fit_transform(X)
+    X_rff = nlf.fit_transform(X)
     end = time.time()
     print(f'Runtime transformation to non-linear features: {end-start}')
 
     # train/test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    X_rff_train, X_rff_test, y_train, y_test = train_test_split(X_rff, y, random_state=42)
 
     # fit and predict rff sequential
     print('\n--- RFF Sequential SVM ---')
     start = time.time()
     rff_sequential_svm = SequentialSVM(learning_rate, regularization)
-    rff_sequential_svm.fit(X_train, y_train)
-    y_predicted_rff_sequential = rff_sequential_svm.predict(X_test)
+    rff_sequential_svm.fit(X_rff_train, y_train)
+    y_predicted_rff_sequential = rff_sequential_svm.predict(X_rff_test)
     end = time.time()
     print("Runtime fit and predict:", end - start)
     print("Accuracy: {}".format(accuracy_score(y_test, y_predicted_rff_sequential)))
@@ -95,13 +95,23 @@ if __name__ == '__main__':
     print('\n--- RFF Parallel SVM ---')
     start = time.time()
     rff_parallel_svm = ParallelSVM(learning_rate, regularization)
-    rff_parallel_svm.fit(X_train, y_train)
-    y_predicted_rff_parallel = rff_parallel_svm.predict(X_test)
+    rff_parallel_svm.fit(X_rff_train, y_train)
+    y_predicted_rff_parallel = rff_parallel_svm.predict(X_rff_test)
     end = time.time()
     print("Runtime fit and predict:", end - start)
     print("Accuracy: {}".format(accuracy_score(y_test, y_predicted_rff_parallel)))
 
     # Plot true labels
-    # two_dim_visualization(X, y, 'True Labels', 'true')
+    two_dim_visualization(X_test, y_test, 'True Labels', 'true')
     # Plot predicted labels
-    # two_dim_visualization(X, y_predicted, 'Predicted Labels', 'predicted')
+    two_dim_visualization(X_test, y_predicted_sklearn_svc, 'Predicted Labels Sklearn SVC',
+                          'output/predicted_sklearn_svc.png')
+    two_dim_visualization(X_test, y_predicted_linear_sequential, 'Predicted Labels Linear Sequential',
+                          'output/predicted_linear_sequential.png')
+    two_dim_visualization(X_test, y_predicted_linear_parallel, 'Predicted Labels Linear Parallel',
+                          'output/predicted_linear_parallel.png')
+    two_dim_visualization(X_test, y_predicted_rff_sequential, 'Predicted Labels RFF Sequential',
+                          'output/predicted_rff_sequential.png')
+    two_dim_visualization(X_test, y_predicted_rff_parallel, 'Predicted Labels RFF Parallel',
+                          'output/predicted_rff_parallel.png')
+
--- utils.py
+++ utils.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
 
 
-def two_dim_visualization(features, labels, algorithm, filename):
+def two_dim_visualization(data, labels, algorithm, path):
+    """
+    Create a 2D scatter plot from the given input data with coloring corresponding to the labelling.
 
-    x1, x2 = features[:, 0], features[:, 1]
+    Parameters
+    ----------
+    data : ndarray of shape (n samples, 2 features)
+        Input data.
+    labels : list (int)
+        List of labels, corresponding to the respective labeling of the data points.
+    path : str
+        String referring to the path, where the plot shall be stored.
+    """
+
+    """
+    x1, x2 = data[:, 0], data[:, 1]
 
     df = pd.DataFrame({'x1': x1, 'x2': x2, 'label': labels})
     groups = df.groupby('label')
@@ -16,7 +31,14 @@ def two_dim_visualization(features, labels, algorithm, filename):
         ax.set_aspect('auto')
         ax.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
         ax.tick_params(axis='y', which='both', left='off', top='off', labelleft='off')
+    """
 
-    ax.set_title("{}".format(algorithm))
-    plt.savefig(f'output/{filename}.png')
+    x1, x2 = data[:, 0], data[:, 1]
+    plt.figure(figsize=(8, 8))
+    plt.scatter(x1, x2, c=labels, cmap='prism', alpha=1.)
+    plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
+    plt.tick_params(axis='y', which='both', left='off', top='off', labelleft='off')
+    plt.xlabel(r"$x_1$", fontsize=14)
+    plt.ylabel(r"$x_2$", fontsize=14)
+    plt.savefig(path, dpi=150, format='png')
     plt.show()
